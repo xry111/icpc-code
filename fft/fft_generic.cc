06c8bf953dde8cbf7ed9de97fbfd020a00a1ae6d
Xi Ruoyao <xry111@xry111.site>
2024-12-23 19:47:14 +0800

    fft: Reduce variable scope
    Spotted by code scanning.

--- fft/fft_generic.cc
+++ fft/fft_generic.cc
@@ -4,11 +4,10 @@ using namespace std;
 template <class T>
 void change(T *a, int n)
 {
-	int i, j, k;
-	for (i = 1, j = n >> 1; i < n - 1; i++) {
+	for (int i = 1, j = n >> 1; i < n - 1; i++) {
 		if (i < j)
 			swap(a[i], a[j]);
-		k = n >> 1;
+		int k = n >> 1;
 		while (j >= k) {
 			j -= k;
 			k /= 2;
